ecfe0e057746ab3312ba9deeb2a59fbddc081de1
Donnadieu <donnadieu.86@gmail.com>
2017-12-19 00:12:06 -0500

    Create migrations

--- db/20171218190836_create_books.rb
+++ db/20171218190836_create_books.rb
@@ -2,7 +2,7 @@ class CreateBooks < ActiveRecord::Migration[5.1]
   def change
     create_table :books do |t|
       t.string :title
-      t.string :author
+      t.string :author_id
       t.integer :rating
     end
   end
