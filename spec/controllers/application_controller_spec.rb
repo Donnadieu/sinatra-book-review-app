282dfe32b2a0ba2f8466561b1e6a02d6768c47a3
Donnadieu <donnadieu.86@gmail.com>
2017-12-29 13:25:32 -0500

    Corrected application controller tests

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -13,24 +13,26 @@ describe ApplicationController do
   describe "Search page" do
     context 'logged in' do
       it 'loads the book search page if logged in' do
-        user = User.create(:name => "User Test", :email => "test@email.com", :password => "123456")
+        user = User.create(:name => "User Test", :email => "test@email.com", :password => "1234567")
 
         visit '/login'
 
         fill_in(:email, :with => "test@email.com")
-        fill_in(:password, :with => "123456")
-        click_button 'submit'
+        fill_in(:password, :with => "1234567")
+        click_button 'Login'
+
+        visit '/books'
         expect(page.status_code).to eq(200)
-        expect(last_response.location).to include("/search")
+        expect(page.current_path).to include("/books")
       end
     end
   end
 
   context 'logged out' do
     it 'does not loads the book search page if logged out' do
-      user = User.create(:name => "User Test", :email => "test@email.com", :password => "123456")
+      user = User.create(:name => "User Test", :email => "test@email.com", :password => "1234567")
 
-      get '/search'
+      get '/books'
 
       expect(last_response.location).to include("/login")
     end
