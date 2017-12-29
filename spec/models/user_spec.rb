3d5abf45c6bb80db3b5e6216fbf718c1aa201324
Donnadieu <donnadieu.86@gmail.com>
2017-12-29 13:09:04 -0500

    Corrected user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,21 +2,22 @@ require 'spec_helper'
 
 describe 'User' do
   before do
-    @user = User.create(:name => "user test", :email => "test123@email.com", :password => "test")
+    @user = User.create(:name => "User Test", :email => "test@email.com", :password => "1234567")
   end
+  # binding.pry
   it 'can slug the username' do
     expect(@user.slug).to eq("user-test")
   end
 
   it 'can find a user based on the slug' do
     slug = @user.slug
-    expect(User.find_by_slug(slug).name).to eq("user test")
+    expect(User.find_by_slug(slug).name).to eq("User Test")
   end
 
   it 'has a secure password' do
 
     expect(@user.authenticate("dog")).to eq(false)
-    expect(@user.authenticate("test")).to eq(@user)
+    expect(@user.authenticate("1234567")).to eq(@user)
 
   end
 end
